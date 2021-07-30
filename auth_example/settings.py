85a75e9d6f14444729a16723f8093dec70a28f91
TRAMPlin30 <77633312+TRAMPlin30@users.noreply.github.com>
2021-07-31 00:54:12 +0300

    Update settings.py

--- auth_example/settings.py
+++ auth_example/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6q1^_wg_g7cj7c@io(mnx5!gqkduzorh&bbap!n^p^(y+q)4*g'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
